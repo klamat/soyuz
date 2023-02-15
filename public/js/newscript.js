dc949003ddaa62705ef2f118e3e50f9ad119353a
rom-kibirev <124060152+rom-kibirev@users.noreply.github.com>
2023-02-15 16:37:25 +0300

    запилил в твоей логике

--- public/js/newscript.js
+++ public/js/newscript.js
@@ -1,24 +1,30 @@
-showSlide = 0;
+window.addEventListener('load', () => {
 
-let n = 0;
-window.currentSlide = (n) => {
-    showSlide(+n);
-};
+    showSlide = 0;
 
-let i = document.querySelectorAll('.slide');
-console.log('\n i', i);
+    let n = 0;
+// window.currentSlide = (n) => {
+//     showSlide(+n);
+// }; Это вызов фунции из DOM, но там ее ничего не вызывает
 
-document.addEventListener("click", function(e) {
-    if (e.target.className=="button-right") {
-        n = n + 1;
-    }
+    let i = document.querySelectorAll('.slide');
+    console.log('\n i', i);
 
-    if (e.target.className=="button-left") {
-        n = n - 1;
-    }
+    document.addEventListener("click", function(e) {
+        if (e.target.className=="button-right") {
+            n = n + 1;
+        }
 
-    showSlide = n;
-    console.log('\n n', n);
-    console.log('\n showSlide', showSlide);
+        if (e.target.className=="button-left") {
+            n = n - 1;
+        }
 
+        if (n < 0) n = (i.length - 1); // переброс на последний слайд
+        else if (n > (i.length - 1)) n = 0; // переброс на первый слайд
+
+        showSlide = n;
+        console.log('\n n', n);
+        console.log('\n showSlide', showSlide);
+
+    });
 });
