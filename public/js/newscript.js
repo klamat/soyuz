094e5e257556288139bf7de087de750c74d232e7
rom-kibirev <124060152+rom-kibirev@users.noreply.github.com>
2023-02-15 16:04:52 +0300

    add logic

--- public/js/newscript.js
+++ public/js/newscript.js
@@ -7,21 +7,40 @@ window.addEventListener('load',() => {
         showSlide(+n);
     };
 
-    let i = document.querySelectorAll('.slide');
-    console.log('\n i', i);
+    let slides = document.querySelectorAll('.slide');
+    console.log('\n slides', slides);
 
-    document.addEventListener("click", function(e) {
-        if (e.target.className=="button-right") {
-            n = n + 1;
-        }
+    // document.addEventListener("click", function(e) {
+    //     if (e.target.className=="button-right") {
+    //         n = n + 1;
+    //     }
+    //
+    //     if (e.target.className=="button-left") {
+    //         n = n - 1;
+    //     }
+    //
+    //     showSlide = n;
+    //     console.log('\n n', n);
+    //     console.log('\n showSlide', showSlide);
+    //
+    // });
 
-        if (e.target.className=="button-left") {
-            n = n - 1;
-        }
-
-        showSlide = n;
-        console.log('\n n', n);
-        console.log('\n showSlide', showSlide);
-
-    });
+    window.clickLeft = () => {
+        showSlide = showSlide - 1;
+        checkLengthSlides(showSlide);
+    }
+    window.clickRight = () => {
+        showSlide = showSlide + 1;
+        checkLengthSlides(showSlide);
+    }
+    
+    const checkLengthSlides = (num) => {
+        
+        console.log('\n ', slides,num);
+        
+        if (num > (slides.length - 1)) showSlide = 0;
+        else if (num < 0) showSlide = (slides.length - 1);
+        
+        console.log('\n ', showSlide);
+    }
 });
