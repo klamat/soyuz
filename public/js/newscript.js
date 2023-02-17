3ec3414ba0e98e8131cc39277487f8635e0eb76c
Griff.Rift <2844544@gmail.com>
2023-02-17 12:56:04 +0300

    рыганина поддалась Рабочий слайдер

--- public/js/newscript.js
+++ public/js/newscript.js
@@ -4,43 +4,33 @@ window.addEventListener('load',() => {
     // console.log('\n slides', slides);
     window.clickLeft = () => { // запускаем функцию clickLeft из DOM дерева
         showSlide = showSlide - 1; // берем объявленную переменную showSlide и вычитаем из нее 1
-        checkLengthSlides(showSlide); // запускаем функцию checkLengthSlides и передаем в нее результат манипуляций с showSlide
+        activeSlide(showSlide); // запускаем функцию activeSlide и передаем в нее результат манипуляций с showSlide
     }
     window.clickRight = () => { // запускаем функцию clickRight из DOM дерева
         showSlide = showSlide + 1; // берем объявленную переменную showSlide и прибавляем к ней 1
-        checkLengthSlides(showSlide); // запускаем функцию checkLengthSlides и передаем в нее результат манипуляций с showSlide
+        activeSlide(showSlide); // запускаем функцию activeSlide и передаем в нее результат манипуляций с showSlide
     }
-    const checkLengthSlides = () => { // запускаем функцию checkLengthSlides из JS, где num, это число переданное в эту функцию из строк 30, 34 - результат манипуляций с showSlide
-
+    window.currentSlide = (n) => {
+        showSlide = +n;
+        activeSlide(showSlide);
+    }
+    const activeSlide = () => { // запускаем функцию activeSlide из JS, где num, это число переданное в эту функцию из строк 30, 34 - результат манипуляций с showSlide
+        let dots = '';
         if (showSlide < 0) showSlide = (slides.length - 1); // иначе если num меньше 0, то showSlide = количеству слайдов
         else if (showSlide > (slides.length - 1)) showSlide = 0; // если num больше количества слайдов, то showSlide = 0
         // console.log('\n end', showSlide); // выводим в консоль результат
         slides.forEach((slide, id) => {
-            slide.classList.add('hidden'); // всем добавил hidden
-            if (showSlide === id) slide.classList.remove('hidden'); // тот что соответствует выбранному числу showSlide убираем hidden
-            console.log('\n slide', slide,);
-        })
-    }
-    let dots = '';
-    slides.forEach((slide, id) =>{
-            dots +='<span type="dots1" class="dot" onclick="currentSlide(' + id + ')"></span>';
-    });
-    console.log(
-        '\n dots', dots,
-    );
-    document.querySelector('.dotsArea').innerHTML = dots;
-    checkLengthSlides(showSlide);
-    let dot = document.querySelectorAll('.dot');
-
-        dot.forEach((el,id) =>{
-            console.log(
-                '\n el', el
-            );
-
-            if (+el === +dot) {
-                dot.classList.add('active')
+            if (showSlide === id) {
+                slide.classList.remove('hidden'); // тот что соответствует выбранному числу showSlide убираем hidden
+                dots +='<span type="dots1" class="dot active" onclick="currentSlide(' + id + ')"></span>';
             }
-    });
-
-
+            else{
+                slide.classList.add('hidden');// всем добавил hidden}
+                dots +='<span type="dots1" class="dot" onclick="currentSlide(' + id + ')"></span>';
+            }
+            console.log('\n slide', slide,);
+        });
+        document.querySelector('.dotsArea').innerHTML = dots;
+    }
+    activeSlide(showSlide);
 });
