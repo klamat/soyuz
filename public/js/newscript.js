188571340ab77d60bc8d797aa63a290803ab5655
RdD <2844544@gmail.com>
2023-02-18 13:08:08 +0300

    Аккордеон от ChatGPT

--- public/js/newscript.js
+++ public/js/newscript.js
@@ -43,3 +43,52 @@ window.addEventListener('load',() => {
 
 /*Accordeon*/
 
+    // document.querySelector('.buttonArea [type="dots1"]').innerHTML = slidesLength;
+    <!--from chatGPT-->
+// select all accordion items
+    const accordionItems = document.querySelectorAll('.accordion-item');
+
+// add click event listener to each accordion item
+    accordionItems.forEach((accordionItem) => {
+        accordionItem.addEventListener('click', function() {
+            // select the content section of the clicked accordion item
+            const accordionContent = this.querySelector('.accordion-content');
+
+            // toggle the active class on the accordion item
+            accordionItem.classList.toggle('active');
+
+            // toggle the rotation of the arrow
+            const accordionArrow = this.querySelector('.accordion-arrow');
+            accordionArrow.classList.toggle('active');
+
+            if (accordionItem.classList.contains('active')) {
+                // if accordion item is active, expand the content section
+                accordionContent.style.maxHeight = accordionContent.scrollHeight + 'px';
+
+                // close any other open accordion items
+                const openAccordionItems = document.querySelectorAll('.accordion-item.active');
+                openAccordionItems.forEach((openAccordionItem) => {
+                    if (openAccordionItem !== accordionItem) {
+                        openAccordionItem.classList.remove('active');
+                        openAccordionItem.querySelector('.accordion-content').style.maxHeight = null;
+                        openAccordionItem.querySelector('.accordion-arrow').classList.remove('active');
+                    }
+                });
+            } else {
+                // if accordion item is inactive, collapse the content section
+                accordionContent.style.maxHeight = null;
+            }
+        });
+    });
+
+// add parallax effect to the background image
+    const parallaxContainer = document.querySelector('.parallax');
+    window.addEventListener('scroll', function() {
+        const yOffset = window.pageYOffset;
+        parallaxContainer.style.backgroundPositionY = yOffset * 0.7 + 'px';
+    });
+
+
+});
+
+
