7924309a6ff1358c803bcebb3ee3f6105a86e4a8
Griff.Rift <2844544@gmail.com>
2023-03-01 15:53:31 +0300

    Панорама-готово

--- public/js/gpt.js
+++ public/js/gpt.js
@@ -1,17 +1,38 @@
 window.addEventListener('DOMContentLoaded', function() {
 
-		window.addEventListener('scroll', () => {
-		let blocks = document.querySelectorAll('.block');
-		blocks.forEach((block) => {
-		let blockTop = block.getBoundingClientRect().top;
-		if (blockTop < window.innerHeight && blockTop > 0) {
-		block.classList.add('right');
+// Создаем сцену
+	var scene = new THREE.Scene();
+
+// Создаем камеру
+	var camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
+	camera.position.z = 5;
+
+// Создаем рендерер
+	var renderer = new THREE.WebGLRenderer();
+	renderer.setSize(window.innerWidth, window.innerHeight);
+	document.getElementById('container').appendChild(renderer.domElement);
+
+// Загружаем модель
+	var loader = new THREE.OBJLoader();
+	loader.load(
+		'../public/docs/obj/2.obj',
+		function (object) {
+			scene.add(object);
+		}
+	);
+
+// Создаем источник освещения
+	var light = new THREE.PointLight(0xffffff, 1, 100);
+	light.position.set(0, 0, 5);
+	scene.add(light);
+
+// Отображаем сцену
+	function animate() {
+		requestAnimationFrame(animate);
+		renderer.render(scene, camera);
 	}
-		else {
-		block.classList.remove('right');
-	}
-	});
-	});
+	animate();
+
 
 
 });
