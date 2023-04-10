e63fbd745b34b0c7b5d07382e2e56782c641c512
Griff.Rift <2844544@gmail.com>
2023-04-10 12:03:57 +0300

    Слайдеры галереи Астра

--- public/js/script.js
+++ public/js/script.js
@@ -166,35 +166,7 @@ window.addEventListener('DOMContentLoaded', function() {
 	const getFields = document.querySelectorAll('.features input');
 	getFields.forEach(field => field.setAttribute('onclick', 'CalculatorCostHandler()'));
 /*-----------------------------------------------------------------------------*/
-	const modal = document.querySelector('.main-modal');
-	const closeButton = document.querySelectorAll('.modal-close');
 
-	const modalClose = () => {
-		modal.classList.remove('fadeIn');
-		modal.classList.add('fadeOut');
-		setTimeout(() => {
-			modal.style.display = 'none';
-		}, 500);
-	}
-
-	window.openModal = () => {
-		modal.classList.remove('fadeOut');
-		modal.classList.add('fadeIn');
-		modal.style.display = 'flex';
-	}
-
-	for (let i = 0; i < closeButton.length; i++) {
-
-		const elements = closeButton[i];
-
-		elements.onclick = (e) => modalClose();
-
-		modal.style.display = 'none';
-
-		window.onclick = function (event) {
-			if (event.target == modal) modalClose();
-		}
-	}
 
 });
 
